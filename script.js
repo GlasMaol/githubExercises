7b91cf91ff604e7cdb38529383585a97384e3dbe
Alistair McCann <allyjmccann@gmail.com>
2024-02-04 18:50:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//första steg för att koppla ihop mappen med repot i main.
+echo "# githubExercises" >> README.md
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+
+git branch -M main
+
+git remote add origin ....
+
+git push -u origin main
+
+
+//skapa dev och en branch till.
+git branch DEV
+    git checkout DEV
+        git push -u origin DEV
+
+git branch feature/one
+    git checkout feature/One
+        git push -u origin feature/One
+
+
